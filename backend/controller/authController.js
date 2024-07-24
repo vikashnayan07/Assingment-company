6b4c57f79846271a8a4af786a7ed0207d5440788
Vikash Nayan <imvikashnayan@gmail.com>
2024-07-25 03:13:20 +0530

    fix

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -41,7 +41,6 @@ const verifyUser = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Decoded Payload:", decoded);
 
-    // Access userId from the data field
     const userId = decoded.data.userId;
 
     req.user = await User.findById(userId);
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "http://localhost:5173",
   })
 );
 
